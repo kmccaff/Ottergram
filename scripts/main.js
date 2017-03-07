605045a522c49233d848534cbba441bf16c7b161
Kevin McCaffrey <kevinericmccaffrey@gmail.com>
2017-03-06 22:27:46 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,8 @@
 var DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
 var DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
 var THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
+var HIDDEN_DETAIL_CLASS = 'hidden-detail';
+var ESC_KEY  = 27;
 
 function setDetails(imageUrl, titleText) {
     'use strict';
@@ -12,17 +14,17 @@ function setDetails(imageUrl, titleText) {
 }
 
 function imageFromThumb(thumbnail) {
-    'use strict'
+    'use strict';
     return thumbnail.getAttribute('data-image-url');
 }
 
 function titleFromThumb(thumbnail) {
-    'use strict'
+    'use strict';
     return thumbnail.getAttribute('data-image-title');
 }
 
 function setDetailsFromThumb(thumbnail) {
-    'use strict'
+    'use strict';
     setDetails(imageFromThumb(thumbnail), titleFromThumb(thumbnail));
 }
 
@@ -31,20 +33,45 @@ function addThumbClickHandler(thumb) {
     thumb.addEventListener('click', function(event) {
         event.preventDefault();
         setDetailsFromThumb(thumb);
+        showDetails();
     });
 }
 
 function getThumbnailsArray() {
-    'use strict'
+    'use strict';
     var thumbnails = document.querySelectorAll(THUMBNAIL_LINK_SELECTOR);
     var thumbnailArray = [].slice.call(thumbnails);
     return thumbnailArray;
 }
 
+function hideDetails() {
+    'use strict';
+    document.body.classList.add(HIDDEN_DETAIL_CLASS);
+}
+
+function showDetails() {
+    'use strict';
+    document.body.classList.remove(HIDDEN_DETAIL_CLASS);
+}
+
+
+function addKeyPressHandler () {
+    'use strict';
+    document.body.addEventListener('keyup', function (event) {
+    event.preventDefault();
+    console.log(event.keyCode);
+    if (event.keyCode === ESC_KEY) {
+        hideDetails();
+    }
+    });
+}
+
 function initializeEvents() {
-    'use strict'
+    'use strict';
     var thumbnails = getThumbnailsArray();
     thumbnails.forEach(addThumbClickHandler);
+    addKeyPressHandler();
 }
 
 initializeEvents();
+
